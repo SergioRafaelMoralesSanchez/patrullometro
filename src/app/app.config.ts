ca83226b08ad4f2f73aee2e5a250b19cbc3a1770
NandaKoreWa <srms98@gmail.com>
2025-02-26 20:38:20 +0100

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfi
