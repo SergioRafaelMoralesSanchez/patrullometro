86f7b623981c1e5fc44381e095ff1cfc7ec0f3f4
NandaKoreWa <srms98@gmail.com>
2025-03-09 22:07:28 +0100

    Fix theme white

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { routes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideRouter } from '@angular/router'
+import Aura from '@primeng/themes/aura'
+import { providePrimeNG } from 'primeng/config'
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,4 +16,4 @@ export const appConfig: ApplicationConfig = {
       }
     })
   ]
-};
+}
